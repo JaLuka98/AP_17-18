0668ed17826b700c4d7f04c0cd83289ffdc5b17a
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2017-10-18 18:46:10 +0200

    added plotting script, plots and calculations for V103

---
+++ V103_biegung_elastischer_staebe/auswertung.py
@@ -0,0 +1,115 @@
+import numpy as np
+import scipy.optimize
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from scipy import optimize
+import matplotlib.pyplot as plt
+from uncertainties import ufloat
+
+
+def f(x, a, b):
+    return a*x+b
+
+
+# Anfang einseitige Einspannung und runder Querschnitt
+x1, D1, D2 = np.genfromtxt('rundeinseitig.txt', unpack=True)
+# in cm, mm, mm
+m = 0.7884  # in kg
+L = 0.54  # in m
+x1 *= 1e-2  # x1 jetzt in m
+d = 8.1  # Durchmesser d des Stabs in mm
+d *= 1e-3  # d jetzt in m
+I = (np.pi/4)*((d/2)**4)  # Flaechentraegheitsmoment für runden Querschnitt  in m^4
+D = D2-D1  # Die Ausgleichsrechnung, um die Biegung in Nullage zu beachten
+xwerte = L*(x1**2)-(x1**3)/3  # Die Gleichung für einseitige Einspannung
+xwerte *= 1e3  # um auf der x-Achse mm zu haben
+
+params, covariance_matrix = optimize.curve_fit(f, xwerte, D)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+print('a =', params[0], '+-', errors[0])
+print('b =', params[1], '+-', errors[1])
+
+a = ufloat(params[0], errors[0])
+
+print('E/10^-9 fuer rund, einseitige Einspannung ist', (m*9.81)/(2*I*a)*1e-9)
+
+plt.plot(xwerte, D, 'rx', label='Messwerte')
+plt.plot(xwerte, f(xwerte, *params), 'b-', label='fit')
+plt.legend()
+plt.xlabel('$(Lx^3-x^3/3)$/mm')
+plt.ylabel('$D(x)$/mm')
+plt.savefig('rundeinseitig.pdf')
+# Ende einseitige Einspannung und runder Querschnitt
+
+
+plt.clf()
+
+
+# Anfang einseitige Einspannung und quadratischer Querschnitt
+x1, D1, D2 = np.genfromtxt('quadratischeinseitig.txt', unpack=True)
+# in cm, mm, mm
+m = 2.3682  # in kg
+L = 0.54  # in m
+x1 *= 1e-2  # x1 jetzt in m
+a = 8.0  # Seitenlaenge a in mm
+a *= 1e-3  # d jetzt in m
+I = (a**4)/12  # Flaechentraegheitsmoment für einen quadratischen Querschnitt in m^4
+D = D2-D1  # Die Ausgleichsrechnung, um die Biegung in Nullage zu beachten
+xwerte = L*(x1**2)-(x1**3)/3  # Die Gleichung für einseitige Einspannung
+xwerte *= 1e3  # um auf der x-Achse mm zu haben
+
+params, covariance_matrix = optimize.curve_fit(f, xwerte, D)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+print('a =', params[0], '+-', errors[0])
+print('b =', params[1], '+-', errors[1])
+
+a = ufloat(params[0], errors[0])
+
+print('E/10^-9 fuer eckig, einseitige Einspannung ist', (m*9.81)/(2*I*a)*1e-9)
+
+plt.plot(xwerte, D, 'rx', label='Messwerte')
+plt.plot(xwerte, f(xwerte, *params), 'b-', label='fit')
+plt.legend()
+plt.xlabel('$(Lx^3-x^3/3)$/mm')
+plt.ylabel('$D(x)$/mm')
+plt.savefig('quadratischeinseitig.pdf')
+# Ende einseitige Einspannung und quadratischer Querschnitt
+
+
+plt.clf()
+
+
+# Anfang beidseitige Einspannung und quadratischer Querschnitt
+x1, D1, D2 = np.genfromtxt('quadratischzweiseitig.txt', unpack=True)
+# in cm, mm, mm
+m = 2.3682  # in kg
+L = 0.54  # in m
+x1 *= 1e-2  # x1 jetzt in m
+a = 8.0  # Seitenlaenge a in mm
+a *= 1e-3  # a jetzt in m
+I = (a**4)/12  # Flaechentraegheitsmoment für einen quadratischen Querschnitt in m^4
+D = D2-D1  # Die Ausgleichsrechnung, um die Biegung in Nullage zu beachten
+xwerte = 3*(L**2)*x1 - 4*x1**3  # Die Gleichung für beidseitige Einspannung
+xwerte *= 1e3
+
+params, covariance_matrix = optimize.curve_fit(f, xwerte, D)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+print('a =', params[0], '+-', errors[0])
+print('b =', params[1], '+-', errors[1])
+
+a = ufloat(params[0], errors[0])
+
+print('E/10^-9 fuer eckig, zweiseitige Einspannung ist', (m*9.81)/(48*I*a)*1e-9)
+
+plt.plot(xwerte, D, 'rx', label='Messwerte')
+plt.plot(xwerte, f(xwerte, *params), 'b-', label='fit')
+plt.legend()
+plt.xlabel('$(3L^2x-4x^2)$/mm')
+plt.ylabel('$D(x)$/mm')
+plt.savefig('quadratischzweiseitig.pdf')
