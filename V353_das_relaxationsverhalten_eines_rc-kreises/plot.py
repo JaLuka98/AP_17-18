cca0020aabfb01ca9ab98bcd94598c51b490076e
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2017-11-16 12:39:09 +0100

    continued working on "auswertung" for V353, done 4a, 4b more or less

--- V353_das_relaxationsverhalten_eines_rc-kreises/plot.py
+++ V353_das_relaxationsverhalten_eines_rc-kreises/plot.py
@@ -12,11 +12,10 @@ def f(x, a, b):
 
 
 # Entladung des Kondensators
-# lege Nullpunkt in obere rechte Ecke das Kästchens unten links
 
 Uc, t = np.genfromtxt('data/abfallende_flanke.txt', unpack=True)  # Uc in mV, t in ms
 U0 = 1090
-
+Uc[0] = 1088
 params, covariance_matrix = optimize.curve_fit(f, t, np.log(Uc/U0))
 
 errors = np.sqrt(np.diag(covariance_matrix))
@@ -28,10 +27,27 @@ a = ufloat(params[0], errors[0])
 b = ufloat(params[1], errors[1])
 
 plt.plot(t, np.log(Uc/U0), 'rx', label='Messwerte')
-plt.plot(t, f(t, *params), 'k-', label='fit')
+plt.plot(t, f(t, *params), 'k-', label='Ausgleichsfunktion')
 plt.xlabel(r'$t/$ms')
 plt.ylabel(r'$\ln{\left(\frac{U_\mathrm{C}}{U_0}\right)}$')
 plt.legend()
 plt.grid()
 plt.tight_layout()
-plt.savefig('Uc.pdf')
+plt.savefig('build/uc.pdf')
+
+plt.clf()
+
+# Berücksichtigung der Frequenzabhängigkeit der Amplitude von Uc
+
+f, Uc = np.genfromtxt('data/amplitude.txt', unpack=True) # Uc in mV, f in Hz
+U0 = 1
+
+
+plt.plot(f, Uc, 'rx', label='Messwerte')
+plt.xscale('log')
+plt.xlabel(r'$f/$Hz')
+plt.ylabel(r'$U/$mV')
+plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.savefig('build/amplitude.pdf')
