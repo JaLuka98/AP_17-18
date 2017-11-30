bd8b2778362cad484137510721d4270234e00b8c
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2017-11-30 11:19:02 +0100

    workworkworkwork

--- V308_spulen_und_magnetfelder/plot.py
+++ V308_spulen_und_magnetfelder/plot.py
@@ -24,8 +24,8 @@ B *= 1e-3 #B/T
 x=np.linspace(1.5, 17.5)
 
 plt.plot(r*1e2, B*1e3, 'rx', label='Messwerte')
-plt.plot(x, (4*np.pi*1e-7*n1*I1*1e3/l1)+0*x, 'b-', label = 'Theoriewert in der Spule')
-plt.plot(x, (4*np.pi*1e-7*n1*I1*1e3/l1)+0*x-0.63, 'g-', label = 'Theoriewert in der Spule - 0,63mT')
+plt.plot(x, (4*np.pi*1e-7*n1*I1*1e3/l1)+0*x, 'b--', label = 'Theoriewert in der Spule')
+plt.plot(x, (4*np.pi*1e-7*n1*I1*1e3/l1)+0*x-0.63, 'g--', label = 'Theoriewert mit angepasstem Nullniveau')
 plt.xlabel(r'$r/$cm')
 plt.ylabel(r'$B/$mT')
 plt.tight_layout()
@@ -44,7 +44,7 @@ B *= 1e-3 #B/T
 x=np.linspace(1.5, 7)
 
 plt.plot(r*1e2, B*1e3, 'rx', label='Messwerte')
-plt.plot(x, (4*np.pi*1e-7*n2*I2*1e3/(np.sqrt(l2*l2+D2*D2)))+0*x - 0.57, 'b-', label = 'Theoriewert in der Spule')
+plt.plot(x, (4*np.pi*1e-7*n2*I2*1e3/(np.sqrt(l2*l2+D2*D2)))+0*x - 0.57, 'b--', label = 'Theoriewert in der Spule')
 plt.xlabel(r'$r/$cm')
 plt.ylabel(r'$B/$mT')
 plt.tight_layout()
@@ -58,26 +58,41 @@ print('Erwartungswert für die kurze Spule:', 4*np.pi*1e-7*n2*I2*1e3/(np.sqrt(l2
 
 I_H1=3 #strom durch spulenpaar
 R=0.0625 #spulenradius
-n=200 #100 Windungen je spule
+n=100 #100 Windungen je spule
 
 r, B = np.genfromtxt('data/spulenpaar_3.txt', unpack=True) #r/cm B/mT
 r *= 1e-2 #r/m
 B *= 1e-3 #B/T
 
-x=np.linspace(-3.125, 3.125)
+x=np.linspace(-0.03125, 0.235)
 
 plt.plot(r*1e2, B*1e3, 'rx', label='Messwerte')
-plt.plot(x, (4*np.pi*1e-7*n*I_H1*1e3*R**2/((2*(R**2))**(3/2)))+0*x, 'b-', label = 'Theoriewert in der Spule')
+plt.plot(x*1e2, 1e3*((4*np.pi*1e-7*n*I_H1)/(2*R))*(1/((((x/R)**2)+(x/R)+(5/4))**(3/2))
++1/((((x/R)**2)-(x/R)+(5/4))**(3/2)))+0*x, 'b--', label = 'Theoriewert')
 plt.xlabel(r'$r/$cm')
 plt.ylabel(r'$B/$mT')
 plt.tight_layout()
 plt.legend()
 plt.grid()
+
+plt.axes([0.2, 0.3, 0.31, 0.31])
+
+plt.plot(r*1e2, B*1e3, 'rx', label='Messwerte')
+plt.plot(x*1e2, 1e3*((4*np.pi*1e-7*n*I_H1)/(2*R))*(1/((((x/R)**2)+(x/R)+(5/4))**(3/2))
++1/((((x/R)**2)-(x/R)+(5/4))**(3/2)))+0*x, 'b--', label = 'Theoriewert')
+plt.xlim(-1, 1)
+plt.ylim(3,4.5)
+plt.xlabel(r'$r/$cm')
+plt.ylabel(r'$B/$mT')
+plt.tight_layout()
+#plt.legend()
+plt.grid()
 plt.savefig('build/spulenpaar_3.pdf')
 
 plt.clf()
 
-print('Erwartungswert für Helmholzspulenpaar mit 3A:', 4*np.pi*1e-7*n*I_H1*1e3*R**2/((2*(R**2))**(3/2)), 'mT')
+print('Erwartungswert für Helmholzspulenpaar mit 3A:', (1e3*(4*np.pi*1e-7*n*I_H1)/(2*R))*(1/((((0/R)**2)+(0/R)+(5/4))**(3/2))
++1/((((0/R)**2)-(0/R)+(5/4))**(3/2))), 'mT')
 
 I_H2=5
 
@@ -86,17 +101,32 @@ r *= 1e-2 #r/m
 B *= 1e-3 #B/T
 
 plt.plot(r*1e2, B*1e3, 'rx', label='Messwerte')
-plt.plot(x, (4*np.pi*1e-7*n*I_H2*1e3*R**2/((2*(R**2))**(3/2)))+0*x, 'b-', label = 'Theoriewert in der Spule')
+plt.plot(x*1e2, 1e3*((4*np.pi*1e-7*n*I_H2)/(2*R))*(1/((((x/R)**2)+(x/R)+(5/4))**(3/2))
++1/((((x/R)**2)-(x/R)+(5/4))**(3/2)))+0*x, 'b--', label = 'Theoriewert')
 plt.xlabel(r'$r/$cm')
 plt.ylabel(r'$B/$mT')
 plt.tight_layout()
 plt.legend()
 plt.grid()
+
+plt.axes([0.2, 0.3, 0.31, 0.31])
+
+plt.plot(r*1e2, B*1e3, 'rx', label='Messwerte')
+plt.plot(x*1e2, 1e3*((4*np.pi*1e-7*n*I_H2)/(2*R))*(1/((((x/R)**2)+(x/R)+(5/4))**(3/2))
++1/((((x/R)**2)-(x/R)+(5/4))**(3/2)))+0*x, 'b--', label = 'Theoriewert')
+plt.xlim(-1, 1)
+plt.ylim(6,7.5)
+plt.xlabel(r'$r/$cm')
+plt.ylabel(r'$B/$mT')
+plt.tight_layout()
+#plt.legend()
+plt.grid()
 plt.savefig('build/spulenpaar_5.pdf')
 
 plt.clf()
 
-print('Erwartungswert für Helmholzspulenpaar mit 5A:', 4*np.pi*1e-7*n*I_H2*1e3*R**2/((2*(R**2))**(3/2)), 'mT')
+print('Erwartungswert für Helmholzspulenpaar mit 5A:', (1e3*(4*np.pi*1e-7*n*I_H2)/(2*R))*(1/((((0/R)**2)+(0/R)+(5/4))**(3/2))
++1/((((0/R)**2)-(0/R)+(5/4))**(3/2))), 'mT')
 
 
 I, B = np.genfromtxt('data/hysterese.txt', unpack=True) #I/a, B/mT
