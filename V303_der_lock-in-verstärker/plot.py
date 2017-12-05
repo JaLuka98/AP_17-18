35ad33ad2a59f5d6458e4173f4105293ed176c27
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2017-12-05 14:45:56 +0100

    data

--- V303_der_lock-in-verstärker/plot.py
+++ V303_der_lock-in-verstärker/plot.py
@@ -5,3 +5,12 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+
+
+#f=1000 Hz
+#U_0=10 mV
+#U_ss=11.4 mv   Amplitude des Rauschens
+#preamlifier: Gain=10
+#filter:Q=2     frequency range=300-1000Hz
+#Lock_in Detector: Gain=5
+#Tiefpass: tao=1-3s     Gain=10
