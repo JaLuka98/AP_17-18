629fbf44973693346dd5da3ba8e19205589554e3
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-01-14 11:54:50 +0100

    finished good part of auswertung for V206

--- V206_die_waermepumpe/plot.py
+++ V206_die_waermepumpe/plot.py
@@ -132,7 +132,7 @@ T0 = 273.15  # in kelvin
 p0 = 1  # in bar
 kappa = 1.14  # in gar nix
 rhovont = ((rho0*T0)/p0) * p_a/T2
-Nmech = 1/(kappa - 1) * (p_b * (p_a/p_b)**(1/kappa) - p_a) * 1/rhovont * massendurchsatz *1e3  # in W
+Nmech = 1/(kappa - 1) * (p_b * (p_a/p_b)**(1/kappa) - p_a) * 1/rhovont * massendurchsatz *1e3 *1e-1 # in W, 1e-1 wegen bar nach Pascal
 
 print('array für p_b', p_b)
 print('array für p_a', p_a)
@@ -141,22 +141,11 @@ print('array für p_a', p_a)
 print(rhovont)
 print(Nmech)
 
-L = ufloat(18830, 190)
-T_1 = ufloat(-0.01170, 0.00030)
-T_2 = ufloat(-0.00949, 0.00030)
-T_3 = ufloat(-0.00729, 0.00030)
-T_4 = ufloat(-0.00509, 0.00030)
-m_1 = (750+16720) * T_1 * 120.9 / L
-m_2 = (750+16720) * T_2 * 120.9 / L
-m_3 = (750+16720) * T_3 * 120.9 / L
-m_4 = (750+16720) * T_4 * 120.9 / L
-#print(m_1, m_2, m_3, m_4)
-
-#Kompressorleistung
-p_a = np.array([440000, 400000, 380000, 370000])
-p_b = np.array([860000, 940000, 1080000, 1150000])
-m = np.array([m_1, m_2, m_3, m_4])
-r = np.array([23100, 21000, 19900, 19400])
-k = 1.14
-N = 1 /0.14 * (p_b * (p_a/p_b)**(1/k)-p_a)*(1/r) * m
-print(N)
+print('rho bei t = 300s ist dann ', rhovont[5])
+print('rho bei t = 900s ist dann ', rhovont[15])
+print('rho bei t = 1200s ist dann ', rhovont[20])
+print('rho bei t = 1800s ist dann ', rhovont[30])
+print('Nmech bei t = 300s ist dann ', Nmech[5])
+print('Nmech bei t = 900s ist dann ', Nmech[15])
+print('Nmech bei t = 1200s ist dann ', Nmech[20])
+print('Nmech bei t = 1800s ist dann ', Nmech[30])
