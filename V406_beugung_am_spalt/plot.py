9dfdbecb78e5ed37ed2e1b517fca6afb398f4672
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-03-31 17:49:45 +0200

    hopefully making V406 works now

--- V406_beugung_am_spalt/plot.py
+++ V406_beugung_am_spalt/plot.py
@@ -1,7 +1,21 @@
-import numpy as np
-import scipy.optimize
-import uncertainties as unc
-import uncertainties.unumpy as unp
-from scipy import optimize
 import matplotlib.pyplot as plt
-from uncertainties import ufloat
+import numpy as np
+
+x = np.linspace(0, 10, 1000)
+y = x ** np.sin(x)
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y, label='Kurve')
+plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+plt.legend(loc='best')
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y, label='Kurve')
+plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
+plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+plt.legend(loc='best')
+
+# in matplotlibrc leider (noch) nicht möglich
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot.pdf')
