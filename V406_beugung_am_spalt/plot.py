0f900e96db2a092a43e357472ebc4eea2ef630f4
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-04-11 19:39:53 +0200

    plotting for V406 is finished

--- V406_beugung_am_spalt/plot.py
+++ V406_beugung_am_spalt/plot.py
@@ -17,6 +17,10 @@ def theory(zeta, A0, zeta_0, b):
     return (A0 * b * np.sinc(b * np.sin((zeta - zeta_0)/(l)) / l_welle))**2
 
 
+def theorycentered(zeta, A0, b):
+    return (A0 * b * np.sinc(b * np.sin((zeta/l) / l_welle)))**2
+
+
 def theory2(zeta, A0, zeta_0, b, spaltentfernung):
     #return (2 * A_0 * np.cos(np.pi*spaltentfernung*np.sin((zeta - zeta_0)/(l))/l_welle) * np.sinc(b * np.sin((zeta - zeta_0)/(l)) / l_welle))**2
     return (2 * np.cos(np.pi*spaltentfernung*np.sin((zeta - zeta_0)/(l))/l_welle))**2*theory(zeta, A0, zeta_0, b)
@@ -41,9 +45,9 @@ I_1 -=I_1d  #Bereinigung Dunkelstrom
 slinspace = np.linspace(-0.017, 0.017, 500)
 
 
-params, covariance_matrix = optimize.curve_fit(theory, s_1, I_1, p0=[0.73, -0.0005, 1e-3])
+params1, covariance_matrix1 = optimize.curve_fit(theory, s_1, I_1, p0=[0.73, -0.0005, 1e-3])
 
-A_0, s_0, b = correlated_values(params, covariance_matrix)
+A_0, s_0, b = correlated_values(params1, covariance_matrix1)
 
 print('Jetzt kommt der erste Einzelspalt')
 print('A_0 =', A_0)
@@ -60,7 +64,7 @@ print('b =', b)
 # s_0 = ufloat(params[1], errors[1])
 # b = ufloat(params[2], errors[2])
 
-plt.plot(slinspace*1e3, theory(slinspace, *params)*1e9, 'k-', label='Ausgleichsfunktion')
+plt.plot(slinspace*1e3, theory(slinspace, *params1)*1e9, 'k-', label='Ausgleichsfunktion')
 
 plt.plot(s_1*1e3, I_1*1e9, 'rx', label='Messwerte')
 
@@ -81,9 +85,9 @@ s_2 *= 1e-3 #s/m
 I_2 *= 1e-9 #I/A
 I_2 -=I_2d  #Bereinigung Dunkelstrom
 
-params, covariance_matrix = optimize.curve_fit(theory, s_2, I_2, p0=[0.73, -0.0005, 1e-3])
+params2, covariance_matrix2 = optimize.curve_fit(theory, s_2, I_2, p0=[0.73, -0.0005, 1e-3])
 
-A_0, s_0, b = correlated_values(params, covariance_matrix)
+A_0, s_0, b = correlated_values(params2, covariance_matrix2)
 
 print('Jetzt kommt der zweite Einzelspalt')
 print('A_0 =', A_0)
@@ -92,7 +96,7 @@ print('b =', b)
 
 plt.plot(s_2*1e3, I_2*1e9, 'rx', label='Messwerte')
 
-plt.plot(slinspace*1e3, theory(slinspace, *params)*1e9, 'k-', label='Ausgleichsfunktion')
+plt.plot(slinspace*1e3, theory(slinspace, *params2)*1e9, 'k-', label='Ausgleichsfunktion')
 
 plt.xlabel(r'$s/$mm')
 plt.ylabel(r'$I/$nA')
@@ -109,9 +113,9 @@ s_3 *= 1e-3 #s/m
 I_3 *= 1e-9 #I/A
 I_3 -=I_3d  #Bereinigung Dunkelstrom
 
-params, covariance_matrix = optimize.curve_fit(theory2, s_3, I_3, p0=[4, -0.0005, 1.5e-3, 1e-3])
+params3, covariance_matrix3 = optimize.curve_fit(theory2, s_3, I_3, p0=[4, -0.0005, 1.5e-3, 1e-3])
 
-A_0, s_0, b, spaltentfernung = correlated_values(params, covariance_matrix)
+A_0, s_0, b, spaltentfernung = correlated_values(params3, covariance_matrix3)
 
 print('Jetzt kommt der Doppelspalt')
 print('A_0 =', A_0)
@@ -121,7 +125,7 @@ print('spaltentfernung =', spaltentfernung)
 
 plt.plot(s_3*1e3, I_3*1e9, 'rx', label='Messwerte')
 
-plt.plot(slinspace*1e3, theory2(slinspace, *params)*1e9, 'k-', label='Ausgleichsfunktion')
+plt.plot(slinspace*1e3, theory2(slinspace, *params3)*1e9, 'k-', label='Ausgleichsfunktion')
 
 plt.xlabel(r'$s/$mm')
 plt.ylabel(r'$I/$nA')
@@ -130,6 +134,49 @@ plt.legend()
 plt.grid()
 plt.savefig('build/doppelspalt.pdf')
 plt.clf()
+
+# Hier sollte man irgendwie die Amplituden gescheit normieren
+faktor = np.max(I_1) / np.max(I_3)
+I_1 = I_1 / faktor
+
+# Dann den Zentrumspunkt shiften
+s_1 += 0.569*1e-3
+s_3 += 0.254*1e-3
+
+# Erneut fitten, da jetzt anders zentriert
+
+#params1, covariance_matrix1 = optimize.curve_fit(theory, s_1, I_1, p0=[5, 0, 1e-3])
+
+#A_0, s_0, b = correlated_values(params1, covariance_matrix1)
+
+#print('Jetzt kommt der korrigierte Einzelspalt')
+#print('A_0 =', A_0)
+#print('s_0 =', s_0)
+#print('b =', b)
+
+#params3, covariance_matrix3 = optimize.curve_fit(theory2, s_3, I_3, p0=[4, -0.2, 1.5e-3, 1e-3])
+
+# Dann in einem Plot geschlossen darstellen
+
+params1[1] = 0
+params3[1] = 0
+print(I_1)
+
+plt.plot(s_1*1e3, I_1*1e9, 'rx', label='Messwerte Einzelspalt')
+plt.plot(slinspace*1e3, theory(slinspace, *params1)*1e9/faktor, 'r-', label='Ausgleichsfunktion Einzelspalt')
+plt.plot(s_3*1e3, I_3*1e9, 'kx', label='Messwerte Doppelspalt')
+plt.plot(slinspace*1e3, theory2(slinspace, *params3)*1e9, 'k-', label='Ausgleichsfunktion Doppelspalt')
+
+plt.xlabel(r'$s/$mm')
+plt.ylabel(r'$I/$nA')
+plt.tight_layout()
+plt.legend()
+plt.legend(loc = 'upper left', fontsize = 'x-small')
+plt.grid()
+plt.savefig('build/doppeleinzel.pdf')
+plt.clf()
+
+I_1 = I_1 * faktor
 
 m = np.zeros((112, 6))
 temp = np.zeros(112-s_1.size)
@@ -152,8 +199,7 @@ I_3 = np.concatenate((I_3, temp))
 m[:,5] = np.zeros(112) + I_3*1e9
 hr = ['$x_1/$mm', '$I_1/$nA', '$x_2/$mm', '$I_2/$nA', '$x_3/$mm', '$I_3/$nA']
 t = matrix2latex(m, headerRow=hr, format='%.2f')
-print(t)
-
+#print(t)
 
 #Was ist das? Ich kommentiere es einfach mal aus ;)
 
