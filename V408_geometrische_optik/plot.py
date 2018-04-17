6d7e59cdd4e115f64fb2ccbefa00493b718c9cdc
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2018-04-17 17:46:01 +0200

    data and durchführung

--- V408_geometrische_optik/plot.py
+++ V408_geometrische_optik/plot.py
@@ -5,3 +5,10 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+
+
+f_1=150 #Brennweite der Bekannten Linse in mm; benutzt für erste Messreihe und Bessel
+f_2=-50  #Brennweite einer Linse bei Abbe in mm
+f_3=50  #Brennweite einer Linse bei Abbe in mm#
+
+G=3 #Gegenstandsgröße in cm
