1581050429e022089fce1a84cfd61d76fb03f69c
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2018-04-25 12:50:04 +0200

    die plots sollten alle drin sein...

--- V501und502_ablenkung_eines_elektronenstrahls/plot.py
+++ V501und502_ablenkung_eines_elektronenstrahls/plot.py
@@ -5,3 +5,162 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+
+
+def linearfit(x, a, b):
+    return a*x+b
+
+#E-Feld Auswertung
+
+U_b1=180    #Beschleunigungsspannungen in V
+U_b2=230
+U_b3=270
+U_b4=300
+U_b5=350
+
+v_1=39.94   #Frequenzen in Hz
+v_2=79.87
+v_3=159.70
+v_4=239.50
+
+A=7.5       #Amplitude in mm
+
+
+print('Die Berechnungen fürs E-Feld:')
+
+U_d1, U_d2, U_d3, U_d4 ,U_d5, D = np.genfromtxt('data/efeld.txt', unpack=True) #U in V und D in mm
+D*=1e-3 #D in m
+
+params, covariance_matrix = optimize.curve_fit(linearfit, U_d1, D)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('D/U_d1 =', params[0], '+-', errors[0])
+E1= ufloat(params[0], errors[0])
+b1 = params[0]  #brauchen wir nachher noch...
+
+plt.plot(U_d1, D, 'rx', mew=0.5, label='Messwerte')
+plt.plot(U_d1, linearfit(U_d1, *params), 'r-',linewidth=0.5, label='Ausgleichsfunktion')
+
+
+params, covariance_matrix = optimize.curve_fit(linearfit, U_d2, D)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('D/U_d2 =', params[0], '+-', errors[0])
+E2 = ufloat(params[0], errors[0])
+b2 = params[0] #brauchen wir nachher noch...
+
+plt.plot(U_d2, D, 'bx',mew=0.5, label='Messwerte')
+plt.plot(U_d2, linearfit(U_d2, *params), 'b-', linewidth=0.5, label='Ausgleichsfunktion')
+
+
+params, covariance_matrix = optimize.curve_fit(linearfit, U_d3, D)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('D/U_d3 =', params[0], '+-', errors[0])
+E3 = ufloat(params[0], errors[0])
+b3 = params[0]
+
+plt.plot(U_d3, D, 'gx',mew=0.5, label='Messwerte')
+plt.plot(U_d3, linearfit(U_d3, *params), 'g-', linewidth=0.5, label='Ausgleichsfunktion')
+
+plt.xlabel(r'$U/$V')
+plt.ylabel(r'$D/$mm')
+plt.tight_layout()
+plt.legend()
+plt.grid()
+plt.savefig('build/ablenkspannung.pdf')
+plt.clf()
+
+#Alle in einem Plot wäre wahrscheinlich ein bisschen viel....
+
+params, covariance_matrix = optimize.curve_fit(linearfit, U_d4, D)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('D/U_d4 =', params[0], '+-', errors[0])
+E4 = ufloat(params[0], errors[0])
+b4 = params[0]
+
+plt.plot(U_d4, D, 'yx', mew=0.5, label='Messwerte')
+plt.plot(U_d4, linearfit(U_d4, *params), 'y-', linewidth=0.5, label='Ausgleichsfunktion')
+
+
+params, covariance_matrix = optimize.curve_fit(linearfit, U_d5, D)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('D/U_d5 =', params[0], '+-', errors[0])
+E5 = ufloat(params[0], errors[0])
+b5 = params[0]
+
+plt.plot(U_d5, D, 'mx', mew=0.5, label='Messwerte')
+plt.plot(U_d5, linearfit(U_d5, *params), 'm-', linewidth=0.5, label='Ausgleichsfunktion')
+
+plt.xlabel(r'$U/$V')
+plt.ylabel(r'$D/$mm')
+plt.tight_layout()
+plt.legend()
+plt.grid()
+plt.savefig('build/ablenkspannung2.pdf')
+plt.clf()
+
+#Im folgenden Abschnitt nehme ich die Fehler nicht mit... geht das vielleicht irgendwie schöner?
+
+E=np.array([b1, b2, b3, b4, b5])
+U_b=np.array([U_b1, U_b2, U_b3, U_b4, U_b5])
+
+params, covariance_matrix = optimize.curve_fit(linearfit, 1/U_b, E)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('a =', params[0], '+-', errors[0])
+a = ufloat(params[0], errors[0])
+
+
+plt.plot(1/U_b, E, 'rx', mew=0.5, label='Messwerte')
+plt.plot(1/U_b, linearfit(1/U_b, *params), 'b-', linewidth=0.5, label='Ausgleichsfunktion')
+
+plt.xlabel(r'$\frac{1}{U_B}$')
+plt.ylabel(r'$E$')
+plt.tight_layout()
+plt.legend()
+plt.grid()
+plt.savefig('build/empfindlichkeiten.pdf')
+plt.clf()
+
+
+
+#B-Feld Auswertung
+print('Jetzt die Berechnungen fürs B-Feld:')
+U_b1=250    #Beschleunigungsspannungen in V
+U_b2=400
+L=0.175     #Strecke, auf der die Elektronen abgelenkt werden in m
+N=20        #Anzahl der Windungen
+
+R=0.3 #Haben wir das gemessen? ich habe jetzt einfach mal ganz grob irgendwas eingesetzt, was ind er größenordnung liegt, um zu gucken ob der plot akzeptabel aussieht...
+
+r=D/(L**2+D**2)
+
+I_1, I_2, D= np.genfromtxt('data/bfeld.txt', unpack=True) #I/A, D/mm
+B_1=4*np.pi*1e7*8/(np.sqrt(125))*N*I_1/R
+B_2=4*np.pi*1e7*8/(np.sqrt(125))*N*I_2/R
+
+params, covariance_matrix = optimize.curve_fit(linearfit, B_1, r)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('a_1 =', params[0], '+-', errors[0])
+a1 = ufloat(params[0], errors[0])
+
+plt.plot(B_1, r, 'bx', mew=0.5, label='Messwerte')
+plt.plot(B_1, linearfit(B_1, *params), 'b-', linewidth=0.5, label='Ausgleichsfunktion')
+
+
+params, covariance_matrix = optimize.curve_fit(linearfit, B_2, r)
+errors = np.sqrt(np.diag(covariance_matrix))
+print('a_2 =', params[0], '+-', errors[0])
+a2 = ufloat(params[0], errors[0])
+
+plt.plot(B_2, r, 'rx', mew=0.5, label='Messwerte')
+plt.plot(B_2, linearfit(B_2, *params), 'r-', linewidth=0.5, label='Ausgleichsfunktion')
+
+plt.xlabel(r'$\frac{D}{L^2+D^2}$')
+plt.ylabel(r'$B$')
+plt.tight_layout()
+plt.legend()
+plt.grid()
+plt.savefig('build/empfindlichkeiten.pdf')
+plt.clf()
+
+
+I_erde=50*1e-3 #Stromstärke in A
+phi=72          #Winkel in Grad
