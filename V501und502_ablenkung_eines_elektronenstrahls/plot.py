dc11138755aee34f14eee48859a56d5938e23f8d
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2018-05-03 08:03:14 +0200

    work...

--- V501und502_ablenkung_eines_elektronenstrahls/plot.py
+++ V501und502_ablenkung_eines_elektronenstrahls/plot.py
@@ -33,7 +33,7 @@ D*=1e-3 #D in m
 
 params, covariance_matrix = optimize.curve_fit(linearfit, U_d1, D)
 errors = np.sqrt(np.diag(covariance_matrix))
-print('D/U_d1 =', params[0], '+-', errors[0])
+print('D/U_d1 =', params[0]*1e3, '+-', errors[0]*1e3)
 E1= ufloat(params[0], errors[0])
 b1 = params[0]  #brauchen wir nachher noch...
 
@@ -43,7 +43,7 @@ plt.plot(U_d1, linearfit(U_d1, *params), 'r-',linewidth=0.5, label='Ausgleichsfu
 
 params, covariance_matrix = optimize.curve_fit(linearfit, U_d2, D)
 errors = np.sqrt(np.diag(covariance_matrix))
-print('D/U_d2 =', params[0], '+-', errors[0])
+print('D/U_d2 =', params[0]*1e3, '+-', errors[0]*1e3)
 E2 = ufloat(params[0], errors[0])
 b2 = params[0] #brauchen wir nachher noch...
 
@@ -53,15 +53,15 @@ plt.plot(U_d2, linearfit(U_d2, *params), 'b-', linewidth=0.5, label='Ausgleichsf
 
 params, covariance_matrix = optimize.curve_fit(linearfit, U_d3, D)
 errors = np.sqrt(np.diag(covariance_matrix))
-print('D/U_d3 =', params[0], '+-', errors[0])
+print('D/U_d3 =', params[0]*1e3, '+-', errors[0]*1e3)
 E3 = ufloat(params[0], errors[0])
 b3 = params[0]
 
 plt.plot(U_d3, D, 'gx',mew=0.5, label='Messwerte')
 plt.plot(U_d3, linearfit(U_d3, *params), 'g-', linewidth=0.5, label='Ausgleichsfunktion')
 
-plt.xlabel(r'$U/$V')
-plt.ylabel(r'$D/$mm')
+plt.xlabel(r'$U_d/$V')
+plt.ylabel(r'$D/$m')
 plt.tight_layout()
 plt.legend()
 plt.grid()
@@ -72,7 +72,7 @@ plt.clf()
 
 params, covariance_matrix = optimize.curve_fit(linearfit, U_d4, D)
 errors = np.sqrt(np.diag(covariance_matrix))
-print('D/U_d4 =', params[0], '+-', errors[0])
+print('D/U_d4 =', params[0]*1e3, '+-', errors[0]*1e3)
 E4 = ufloat(params[0], errors[0])
 b4 = params[0]
 
@@ -82,15 +82,15 @@ plt.plot(U_d4, linearfit(U_d4, *params), 'y-', linewidth=0.5, label='Ausgleichsf
 
 params, covariance_matrix = optimize.curve_fit(linearfit, U_d5, D)
 errors = np.sqrt(np.diag(covariance_matrix))
-print('D/U_d5 =', params[0], '+-', errors[0])
+print('D/U_d5 =', params[0]*1e3, '+-', errors[0]*1e3)
 E5 = ufloat(params[0], errors[0])
 b5 = params[0]
 
 plt.plot(U_d5, D, 'mx', mew=0.5, label='Messwerte')
 plt.plot(U_d5, linearfit(U_d5, *params), 'm-', linewidth=0.5, label='Ausgleichsfunktion')
 
-plt.xlabel(r'$U/$V')
-plt.ylabel(r'$D/$mm')
+plt.xlabel(r'$U_d/$V')
+plt.ylabel(r'$D/$m')
 plt.tight_layout()
 plt.legend()
 plt.grid()
@@ -119,17 +119,31 @@ print('a =', params[0], '+-', errors[0])
 a = ufloat(params[0], errors[0])
 
 
-plt.plot(1/U_b, E, 'rx', mew=0.5, label='Messwerte')
-plt.plot(1/U_b, linearfit(1/U_b, *params), 'b-', linewidth=0.5, label='Ausgleichsfunktion')
+plt.plot((1/U_b)*1e3, E*1e3, 'rx', mew=0.5, label='Messwerte')
+plt.plot((1/U_b)*1e3, linearfit(1/U_b, *params)*1e3, 'b-', linewidth=0.5, label='Ausgleichsfunktion')
 
-plt.xlabel(r'$\frac{1}{U_B}$')
-plt.ylabel(r'$E$')
+plt.xlabel(r'$\frac{1}{U_b}/\frac{1}{V}\cdot 10^{-3}$')
+plt.ylabel(r'$\frac{D}{U_d}/\frac{m}{V}\cdot 10^{-3}$')
 plt.tight_layout()
 plt.legend()
 plt.grid()
 plt.savefig('build/empfindlichkeiten.pdf')
 plt.clf()
 
+#Diese komische Größe berechnen
+
+#Da es um y-Ablenkung geht, nehme ich auch mal die Werte dafür aus der Zeichnung
+p=1.9 #cm
+L=14.3 #cm
+d=0.38  #cm
+
+p*=1e-2 #alles in meter
+L*=1e-2
+d*=1e-2
+
+a_zeichnung=p*L/(2*d)
+
+print('Die Größe pL/(2d) ist a_zeichnung=', a_zeichnung)
 
 
 #B-Feld Auswertung
@@ -152,8 +166,8 @@ errors = np.sqrt(np.diag(covariance_matrix))
 print('a_1 =', params[0], '+-', errors[0])
 a1 = ufloat(params[0], errors[0])
 
-plt.plot(B_1, r, 'bx', mew=0.5, label='Messwerte')
-plt.plot(B_1, linearfit(B_1, *params), 'b-', linewidth=0.5, label='Ausgleichsfunktion')
+plt.plot(B_1*1e6, r, 'bx', mew=0.5, label='Messwerte')
+plt.plot(B_1*1e6, linearfit(B_1, *params), 'b-', linewidth=0.5, label='Ausgleichsfunktion')
 
 
 params, covariance_matrix = optimize.curve_fit(linearfit, B_2, r)
@@ -161,16 +175,19 @@ errors = np.sqrt(np.diag(covariance_matrix))
 print('a_2 =', params[0], '+-', errors[0])
 a2 = ufloat(params[0], errors[0])
 
-plt.plot(B_2, r, 'rx', mew=0.5, label='Messwerte')
-plt.plot(B_2, linearfit(B_2, *params), 'r-', linewidth=0.5, label='Ausgleichsfunktion')
+plt.plot(B_2*1e6, r, 'rx', mew=0.5, label='Messwerte')
+plt.plot(B_2*1e6, linearfit(B_2, *params), 'r-', linewidth=0.5, label='Ausgleichsfunktion')
 
-plt.xlabel(r'$B$')
-plt.ylabel(r'$\frac{D}{L^2+D^2}$')
+plt.xlabel(r'$B$/µT')
+plt.ylabel(r'$r$/m')
 plt.tight_layout()
 plt.legend()
 plt.grid()
 plt.savefig('build/bfeld.pdf')
 plt.clf()
+
+print('e/m_1 ergibt:', a1**2*8*U_b1)
+print('e/m_1 ergibt:', a2**2*8*U_b2)
 
 hr = ['$I_1/$A','$B_1/$µT','$I_2/$A','$B_2/$µT', '$D/$mm','$r/$m' ]
 m = np.zeros((9, 6))
@@ -183,5 +200,13 @@ m[:,5] = r
 t = matrix2latex(m, headerRow=hr, format='%.2f')
 print(t)
 
+#Das Erdmagnetfeld
+
 I_erde=50*1e-3 #Stromstärke in A
-phi=72          #Winkel in Grad
+alpha=72          #Winkel in Grad
+phi=alpha*2*np.pi/360
+
+B_spulen=4*np.pi*1e-7*8/(np.sqrt(125))*N*I_erde/R
+print('B_gegen=', B_spulen)
+B_erde=B_spulen/np.cos(phi)
+print('Das Erdnamagnetfeld: B_erde=', B_erde)
