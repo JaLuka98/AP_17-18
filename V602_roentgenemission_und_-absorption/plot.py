7554c311d9c7301a438008feab5a6899232a2fe2
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-05-26 17:50:46 +0200

    my part of auswertung for V602 is (hopefully) done (well)

--- V602_roentgenemission_und_-absorption/plot.py
+++ V602_roentgenemission_und_-absorption/plot.py
@@ -5,14 +5,17 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+from uncertainties import correlated_values
 from matrix2latex import matrix2latex
 
+def linfit(x,a,b):
+    return a*x+b
 
 #Bragg
 
 alpha, I = np.genfromtxt('data/bragg.txt', unpack=True)
 plt.plot(alpha, I, 'rx', mew=0.5)
-plt.xlabel(r'$\alpha/$Grad')
+plt.xlabel(r'$\alpha/$°')
 plt.ylabel(r'$I$')
 plt.tight_layout()
 plt.legend()
@@ -20,7 +23,7 @@ plt.grid()
 plt.savefig('build/bragg.pdf')
 plt.clf()
 
-# hr = ['$\alpha/$Grad', '$I$']
+# hr = ['$\alpha/$°', '$I$']
 # m = np.zeros((np.size(I), 2))
 # m[: ,0] = alpha
 # m[:, 1] = I
@@ -32,7 +35,7 @@ plt.clf()
 zweitheta, I = np.genfromtxt('data/emission.txt', unpack=True)
 zweitheta[0] = 8.0 # dummer scheiß geht nur so
 plt.plot(zweitheta, I, 'rx', mew=0.5)
-plt.xlabel(r'$2 \cdot \theta/$Grad')
+plt.xlabel(r'$2 \cdot \theta/$°')
 plt.ylabel(r'$I$')
 plt.tight_layout()
 plt.legend()
@@ -40,7 +43,7 @@ plt.grid()
 plt.savefig('build/emission.pdf')
 plt.clf()
 
-hr = ['$2 \cdot \theta/$Grad', '$I$']
+hr = ['$2 \cdot \theta/$°', '$I$']
 m = np.zeros((np.size(alpha), 4))
 m[:, 0] = zweitheta[0:np.size(alpha)]
 m[:, 1] = I[0:np.size(alpha)]
@@ -60,7 +63,7 @@ print(t)
 
 zweitheta, I = np.genfromtxt('data/strontium.txt', unpack=True) #f/kHz, U/V
 plt.plot(zweitheta, I, 'rx', mew=0.5)
-plt.xlabel(r'$2 \cdot \theta/$Grad')
+plt.xlabel(r'$2 \cdot \theta/$°')
 plt.ylabel(r'$I$')
 plt.tight_layout()
 plt.legend()
@@ -72,7 +75,7 @@ plt.clf()
 
 zweitheta, I = np.genfromtxt('data/zirkonium.txt', unpack=True) #f/kHz, U/V
 plt.plot(zweitheta, I, 'rx', mew=0.5)
-plt.xlabel(r'$2 \cdot \theta/$Grad')
+plt.xlabel(r'$2 \cdot \theta/$°')
 plt.ylabel(r'$I$')
 plt.tight_layout()
 plt.legend()
@@ -84,11 +87,12 @@ plt.clf()
 
 zweitheta, I = np.genfromtxt('data/zink.txt', unpack=True) #f/kHz, U/V
 plt.plot(zweitheta, I, 'rx', mew=0.5)
-plt.xlabel(r'$2 \cdot \theta/$Grad')
+plt.xlabel(r'$2 \cdot \theta/$°')
 plt.ylabel(r'$I$')
 plt.tight_layout()
 plt.legend()
 plt.grid()
+plt.axis((31.75, 39.3, 27.5, 60))
 plt.savefig('build/zink.pdf')
 plt.clf()
 
@@ -96,7 +100,7 @@ plt.clf()
 
 zweitheta, I = np.genfromtxt('data/brom.txt', unpack=True) #f/kHz, U/V
 plt.plot(zweitheta, I, 'rx', mew=0.5)
-plt.xlabel(r'$2 \cdot \theta/$Grad')
+plt.xlabel(r'$2 \cdot \theta/$°')
 plt.ylabel(r'$I$')
 plt.tight_layout()
 plt.legend()
@@ -104,11 +108,36 @@ plt.grid()
 plt.savefig('build/brom.pdf')
 plt.clf()
 
+# Bestimmung der Rydbergenergie durch Moseley
+
+Z = [30,35,38,40]
+zlinspace = np.linspace(20, 50, 500)
+E_k = [9.575856772, 13.42955074, 15.84713652, 17.05114588]
+params, covariance_matrix = optimize.curve_fit(linfit, Z, np.sqrt(E_k))
+
+a, b = correlated_values(params, covariance_matrix)
+
+print('Fit zur Rydbergenergie')
+print('a =', a)
+print('a^2 =', a**2*1e3)
+print('b =', b)
+
+plt.plot(Z, np.sqrt(E_k), 'rx', mew=0.5)
+plt.plot(zlinspace, linfit(zlinspace, *params), 'r-')
+plt.xlabel(r'$Z$')
+plt.ylabel(r'$\sqrt{E_K}/\mathrm{\sqrt{keV}}')
+plt.tight_layout()
+plt.legend()
+plt.grid()
+plt.axis((27.5, 42.5, 2.5, 4.75))
+plt.savefig('build/rydberg.pdf')
+plt.clf()
+
 #Gold
 
 zweitheta, I = np.genfromtxt('data/gold.txt', unpack=True) #f/kHz, U/V
 plt.plot(zweitheta, I, 'rx', mew=0.5)
-plt.xlabel(r'$2 \cdot \theta/$Grad')
+plt.xlabel(r'$2 \cdot \theta/$°')
 plt.ylabel(r'$I$')
 plt.tight_layout()
 plt.legend()
