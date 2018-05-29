ffc472b6b037af43c662dc427b88fdbe0bf5501c
Jan Lukas Schubert <jan-lukas.schubert@tu-dortmund.de>
2018-05-29 18:06:42 +0200

    Durchführung und daten

--- V704_absorption_von_gamma-_und_beta-strahlung/plot.py
+++ V704_absorption_von_gamma-_und_beta-strahlung/plot.py
@@ -5,3 +5,11 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+
+#Nullmessung
+t=1000  #s
+N=1028
+
+#Messung des Strahlers ohne Absorber
+t=60 #s
+N=8808
