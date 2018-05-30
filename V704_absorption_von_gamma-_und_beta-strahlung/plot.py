741f6cb467b7d4933dc466692a91c1c179876fa6
Jan Lukas Späh <janlukas.spaeh@tu-dortmund.de>
2018-05-30 21:28:45 +0200

    quicksave

--- V704_absorption_von_gamma-_und_beta-strahlung/plot.py
+++ V704_absorption_von_gamma-_und_beta-strahlung/plot.py
@@ -5,11 +5,33 @@ import uncertainties.unumpy as unp
 from scipy import optimize
 import matplotlib.pyplot as plt
 from uncertainties import ufloat
+from uncertainties import correlated_values
+from matrix2latex import matrix2latex
 
 #Nullmessung
-t=1000  #s
-N=1028
+tno=1000  #s
+Nno=1028
+Ano = Nno/tno # Nullaktivität
 
 #Messung des Strahlers ohne Absorber
-t=60 #s
-N=8808
+tnm=60 #s
+Nnm=8808
+
+# Zink
+
+d, t, N = np.genfromtxt('data\zink.txt', unpack=True)  # d in mm, t in s
+Azink = N/t - Ano
+
+# Az = N/t - An0
+hr = ['$d/$mm', '$t/$s', '$N_\mathrm{z} \pm \sigma N_\mathrm{z}$', '$(A_\mathrm{z}) \pm \sigma N_\mathrm{z})\cdot $s']
+m = np.zeros((np.size(d), 4))
+m[:, 0] = d
+m[:, 1] = t
+m[:, 2] = N
+m[:, 3] = Azink
+table = matrix2latex(m, headerRow=hr, format='%.2f')
+print(table)
+
+sigmaAno = np.sqrt(Nno)/tno
+sigmaA = np.sqrt(N)/t
+print(sigmaA-sigmaAno)
